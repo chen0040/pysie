f1f19a22174d077883e1042b74ec6314bcbb42fa
xschen <xs0040@gmail.com>
2017-05-31 09:42:13 +0800

    Implement the proportion diff and mean diff sampling distribution and unit tests

--- pysie/stats/distributions.py
+++ pysie/stats/distributions.py
@@ -19,6 +19,8 @@ class MeanSamplingDistribution(object):
     point_estimate = None
     distribution_family = None
     df = None
+    sample_sd = None
+    sample_size = None
 
     def __init__(self, sample_distribution=None, sample_mean=None, sample_sd=None, sample_size=None):
         if sample_mean is not None:
@@ -53,6 +55,83 @@ class MeanSamplingDistribution(object):
         if self.distribution_family == DistributionFamily.normal:
             z = norm.ppf(q)
             pf = z * self.standard_error
+            return self.point_estimate - pf, self.point_estimate + pf
+        else:
+            t_df = t.ppf(q, self.df)
+            pf = t_df * self.standard_error + self.point_estimate
+            return self.point_estimate - pf, self.point_estimate + pf
+
+
+class MeanDiffSamplingDistribution(object):
+    grp1_sample_distribution = None
+    grp2_sample_distribution = None
+    grp1_point_estimate = None
+    grp2_point_estimate = None
+    grp1_sample_sd = None
+    grp2_sample_sd = None
+    grp1_sample_size = None
+    grp2_sample_size = None
+    distribution_family = None
+    df = None
+    point_estimate = None
+
+    def __init__(self, grp1_sample_distribution=None, grp1_sample_mean=None, grp1_sample_sd=None, grp1_sample_size=None,
+                 grp2_sample_distribution=None, grp2_sample_mean=None, grp2_sample_sd=None, grp2_sample_size=None):
+        self.build_grp1(grp1_sample_distribution, grp1_sample_mean, grp1_sample_sd, grp1_sample_size)
+        self.build_grp2(grp2_sample_distribution, grp2_sample_mean, grp2_sample_sd, grp2_sample_size)
+
+        self.standard_error = self.calculate_standard_error()
+
+        self.df = min(self.grp1_sample_size - 1.0, self.grp2_sample_size - 1.0)
+        self.point_estimate = self.grp1_point_estimate - self.grp2_point_estimate
+
+        if self.grp1_sample_size < 30 or self.grp2_sample_size < 30:
+            self.distribution_family = DistributionFamily.student_t
+        else:
+            self.distribution_family = DistributionFamily.normal
+
+    def build_grp1(self, grp1_sample_distribution=None, grp1_sample_mean=None, grp1_sample_sd=None, grp1_sample_size=None):
+        if grp1_sample_mean is not None:
+            self.grp1_point_estimate = grp1_sample_mean
+
+        if grp1_sample_sd is not None:
+            self.grp1_sample_sd = grp1_sample_sd
+
+        if grp1_sample_size is not None:
+            self.grp1_sample_size = grp1_sample_size
+
+        if grp1_sample_distribution is not None:
+            self.grp1_sample_distribution = grp1_sample_distribution
+            self.grp1_point_estimate = grp1_sample_distribution.mean
+            self.grp1_sample_sd = grp1_sample_distribution.sd
+            self.grp1_sample_size = grp1_sample_distribution.sample_size
+            
+    def build_grp2(self, grp2_sample_distribution=None, grp2_sample_mean=None, grp2_sample_sd=None, grp2_sample_size=None):
+        if grp2_sample_mean is not None:
+            self.grp2_point_estimate = grp2_sample_mean
+
+        if grp2_sample_sd is not None:
+            self.grp2_sample_sd = grp2_sample_sd
+
+        if grp2_sample_size is not None:
+            self.grp2_sample_size = grp2_sample_size
+
+        if grp2_sample_distribution is not None:
+            self.grp2_sample_distribution = grp2_sample_distribution
+            self.grp2_point_estimate = grp2_sample_distribution.mean
+            self.grp2_sample_sd = grp2_sample_distribution.sd
+            self.grp2_sample_size = grp2_sample_distribution.sample_size
+
+    def calculate_standard_error(self):
+        return math.sqrt(self.grp1_sample_sd * self.grp1_sample_sd / self.grp1_sample_size + 
+                         self.grp2_sample_sd * self.grp2_sample_sd / self.grp2_sample_size)
+
+    def confidence_interval(self, confidence_level):
+        q = 1 - (1 - confidence_level) / 2
+        if self.distribution_family == DistributionFamily.normal:
+            z = norm.ppf(q)
+            pf = z * self.standard_error
+            
             return self.point_estimate - pf, self.point_estimate + pf
         else:
             t_df = t.ppf(q, self.df)
@@ -97,12 +176,12 @@ class ProportionSamplingDistribution(object):
 
     def simulate(self):
         self.simulated_proportions = [0] * 1000
-        for iter in range(1000):
+        for i in range(1000):
             count = 0
             for trials in range(self.sample_size):
                 if random.random() <= self.point_estimate:
                     count += 1
-            self.simulated_proportions[iter] = float(count) / self.sample_size
+            self.simulated_proportions[i] = float(count) / self.sample_size
         self.simulated_proportions = sorted(self.simulated_proportions)
 
     def confidence_interval(self, confidence_level):
@@ -115,4 +194,114 @@ class ProportionSamplingDistribution(object):
             threshold1 = int(1000 * (1 - confidence_level) / 2)
             threshold2 = int(1000 * q)
             return self.simulated_proportions[threshold1], self.simulated_proportions[threshold2]
+        
+        
+class ProportionDiffSamplingDistribution(object):
+    grp1_sample_distribution = None
+    grp2_sample_distribution = None
+    grp1_point_estimate = None
+    grp2_point_estimate = None
+    distribution_family = None
+    grp1_sample_size = None
+    grp2_sample_size = None
+    categorical_value = None
+    standard_error = None
+    grp1_simulated_proportions = None
+    grp2_simulated_proportions = None
+    diff_simulated_proportions = None
+    point_estimate = None
+
+    def __init__(self, categorical_value=None,
+                 grp1_sample_distribution=None, grp1_sample_proportion=None, grp1_sample_size=None,
+                 grp2_sample_distribution=None, grp2_sample_proportion=None, grp2_sample_size=None):
+        if categorical_value is not None:
+            self.categorical_value = categorical_value
+            
+        self.build_grp1(grp1_sample_distribution, grp1_sample_proportion, grp1_sample_size)
+        self.build_grp2(grp2_sample_distribution, grp2_sample_proportion, grp2_sample_size)
+
+        if not self.is_clt_held():
+            self.distribution_family = DistributionFamily.simulation
+            self.simulate()
+        else:
+            self.distribution_family = DistributionFamily.normal
+            self.standard_error = self.calculate_standard_error()
+
+        self.point_estimate = self.grp1_point_estimate - self.grp2_point_estimate
+            
+    def calculate_standard_error(self):
+        return math.sqrt(self.grp1_point_estimate * (1 - self.grp1_point_estimate) / self.grp2_sample_size +
+                         self.grp2_point_estimate * (1 - self.grp2_point_estimate) / self.grp2_sample_size)
+
+    def is_clt_held(self):
+        if self.grp1_sample_size * self.grp1_point_estimate < 10:
+            return False
+        if self.grp1_sample_size * (1 - self.grp1_point_estimate) < 10:
+            return False
+        if self.grp2_sample_size * self.grp2_point_estimate < 10:
+            return False
+        if self.grp2_sample_size * (1 - self.grp2_point_estimate) < 10:
+            return False
+        return True
+
+    def build_grp1(self, grp1_sample_distribution=None, grp1_sample_proportion=None, grp1_sample_size=None):
+        if grp1_sample_proportion is not None:
+            self.grp1_point_estimate = grp1_sample_proportion
+
+        if grp1_sample_size is not None:
+            self.grp1_sample_size = grp1_sample_size
+
+        if grp1_sample_distribution is not None:
+            self.grp1_sample_distribution = grp1_sample_distribution
+            self.grp1_point_estimate = grp1_sample_distribution.proportion
+            self.categorical_value = grp1_sample_distribution.categorical_value
+            self.grp1_sample_size = grp1_sample_distribution.sample_size
+
+    def build_grp2(self, grp2_sample_distribution=None, grp2_sample_proportion=None, grp2_sample_size=None):
+        if grp2_sample_proportion is not None:
+            self.grp2_point_estimate = grp2_sample_proportion
+
+        if grp2_sample_size is not None:
+            self.grp2_sample_size = grp2_sample_size
+
+        if grp2_sample_distribution is not None:
+            self.grp2_sample_distribution = grp2_sample_distribution
+            self.grp2_point_estimate = grp2_sample_distribution.proportion
+            self.categorical_value = grp2_sample_distribution.categorical_value
+            self.grp2_sample_size = grp2_sample_distribution.sample_size
+            
+    def simulate(self):
+        self.grp1_simulated_proportions = ProportionDiffSamplingDistribution.simulate_grp(self.grp1_point_estimate,
+                                                                                          self.grp1_sample_size)
+        self.grp2_simulated_proportions = ProportionDiffSamplingDistribution.simulate_grp(self.grp2_point_estimate,
+                                                                                          self.grp2_sample_size)
+
+        self.diff_simulated_proportions = [0] * 1000;
+        for i in range(1000):
+            self.diff_simulated_proportions[i] = self.grp1_simulated_proportions[i] - self.grp2_simulated_proportions[i]
+        
+    @staticmethod
+    def simulate_grp(proportion, sample_size):
+        simulated_proportions = [0] * 1000
+        for iter in range(1000):
+            count = 0
+            for trials in range(sample_size):
+                if random.random() <= proportion:
+                    count += 1
+            simulated_proportions[iter] = float(count) / sample_size
+
+        return sorted(simulated_proportions)
+    
+    def confidence_interval(self, confidence_level):
+        q = 1 - (1 - confidence_level) / 2
+        if self.distribution_family == DistributionFamily.normal:
+            z = norm.ppf(q)
+            pf = z * self.standard_error
+            return self.point_estimate - pf, self.point_estimate + pf
+        else:
+            threshold1 = int(1000 * (1 - confidence_level) / 2)
+            threshold2 = int(1000 * q)
+            return self.diff_simulated_proportions[threshold1], self.diff_simulated_proportions[threshold2]
+
+
 
--- tests/stats/distribution_unit_test.py
+++ tests/stats/distribution_unit_test.py
@@ -2,17 +2,18 @@ import unittest
 
 from numpy.random import normal, random
 
-from pysie.stats.distributions import MeanSamplingDistribution, DistributionFamily, ProportionSamplingDistribution
+from pysie.stats.distributions import MeanSamplingDistribution, DistributionFamily, ProportionSamplingDistribution, \
+    MeanDiffSamplingDistribution, ProportionDiffSamplingDistribution
 from pysie.stats.samples import Sample, SampleDistribution
 
 
 class MeanSamplingDistributionUnitTest(unittest.TestCase):
-
     def test_confidence_interval_with_sample_stats_normal(self):
         sample_mean = 0
         sample_sd = 1
         sample_size = 31
-        sampling_distribution = MeanSamplingDistribution(sample_mean=sample_mean, sample_sd=sample_sd, sample_size=sample_size)
+        sampling_distribution = MeanSamplingDistribution(sample_mean=sample_mean, sample_sd=sample_sd,
+                                                         sample_size=sample_size)
         self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.normal)
         print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
               + ', standard_error=' + str(sampling_distribution.standard_error) + ')')
@@ -37,7 +38,8 @@ class MeanSamplingDistributionUnitTest(unittest.TestCase):
         sample_mean = 0
         sample_sd = 1
         sample_size = 29
-        sampling_distribution = MeanSamplingDistribution(sample_mean=sample_mean, sample_sd=sample_sd, sample_size=sample_size)
+        sampling_distribution = MeanSamplingDistribution(sample_mean=sample_mean, sample_sd=sample_sd,
+                                                         sample_size=sample_size)
         self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.student_t)
         print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
               + ', standard_error = ' + str(sampling_distribution.standard_error) + ')')
@@ -59,8 +61,91 @@ class MeanSamplingDistributionUnitTest(unittest.TestCase):
         print('confidence interval for 95% confidence level: ' + str(sampling_distribution.confidence_interval(0.95)))
 
 
-class ProportionSamplingDistributionUnitTest(unittest.TestCase):
+class MeanDiffSamplingDistributionUnitTest(unittest.TestCase):
+    def test_confidence_interval_with_sample_stats_normal(self):
+        grp1_sample_mean = 0
+        grp1_sample_sd = 1
+        grp1_sample_size = 31
+        grp2_sample_mean = 0.001
+        grp2_sample_sd = 2.1
+        grp2_sample_size = 36
+        sampling_distribution = MeanDiffSamplingDistribution(grp1_sample_mean=grp1_sample_mean,
+                                                             grp1_sample_sd=grp1_sample_sd,
+                                                             grp1_sample_size=grp1_sample_size,
+                                                             grp2_sample_mean=grp2_sample_mean,
+                                                             grp2_sample_sd=grp2_sample_sd,
+                                                             grp2_sample_size=grp2_sample_size)
+        self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.normal)
+        print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
+              + ', standard_error=' + str(sampling_distribution.standard_error) + ')')
+        print('confidence interval for 95% confidence level: ' + str(sampling_distribution.confidence_interval(0.95)))
 
+    def test_confidence_interval_with_sample_normal(self):
+        grp1_mu = 0.0
+        grp1_sigma = 1.0
+        grp1_sample_size = 31
+        grp1_sample = Sample()
+
+        grp2_mu = 0.09
+        grp2_sigma = 2.0
+        grp2_sample_size = 36
+        grp2_sample = Sample()
+
+        for i in range(grp1_sample_size):
+            grp1_sample.add_numeric(normal(grp1_mu, grp1_sigma))
+
+        for i in range(grp2_sample_size):
+            grp2_sample.add_numeric(normal(grp2_mu, grp2_sigma))
+
+        sampling_distribution = MeanDiffSamplingDistribution(grp1_sample_distribution=SampleDistribution(grp1_sample),
+                                                             grp2_sample_distribution=SampleDistribution(grp2_sample))
+        self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.normal)
+        print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
+              + ', standard_error = ' + str(sampling_distribution.standard_error) + ')')
+        print('confidence interval for 95% confidence level: ' + str(sampling_distribution.confidence_interval(0.95)))
+
+    def test_confidence_interval_with_sample_stats_student(self):
+        grp1_sample_mean = 0
+        grp1_sample_sd = 1
+        grp1_sample_size = 29
+        grp2_sample_mean = 0.001
+        grp2_sample_sd = 1.3
+        grp2_sample_size = 24
+        sampling_distribution = MeanDiffSamplingDistribution(grp1_sample_mean=grp1_sample_mean,
+                                                             grp1_sample_sd=grp1_sample_sd,
+                                                             grp1_sample_size=grp1_sample_size,
+                                                             grp2_sample_mean=grp2_sample_mean,
+                                                             grp2_sample_sd=grp2_sample_sd,
+                                                             grp2_sample_size=grp2_sample_size)
+        self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.student_t)
+        print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
+              + ', standard_error = ' + str(sampling_distribution.standard_error) + ')')
+        print('confidence interval for 95% confidence level: ' + str(sampling_distribution.confidence_interval(0.95)))
+
+    def test_confidence_interval_with_sample_student(self):
+        grp1_mu = 0.0
+        grp1_sigma = 1.0
+        grp1_sample_size = 29
+        grp1_sample = Sample()
+        grp2_mu = 0.08
+        grp2_sigma = 1.1
+        grp2_sample_size = 27
+        grp2_sample = Sample()
+
+        for i in range(grp1_sample_size):
+            grp1_sample.add_numeric(normal(grp1_mu, grp1_sigma))
+        for i in range(grp2_sample_size):
+            grp2_sample.add_numeric(normal(grp2_mu, grp2_sigma))
+
+        sampling_distribution = MeanDiffSamplingDistribution(grp1_sample_distribution=SampleDistribution(grp1_sample),
+                                                             grp2_sample_distribution=SampleDistribution(grp2_sample))
+        self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.student_t)
+        print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
+              + ', standard_error = ' + str(sampling_distribution.standard_error) + ')')
+        print('confidence interval for 95% confidence level: ' + str(sampling_distribution.confidence_interval(0.95)))
+
+
+class ProportionSamplingDistributionUnitTest(unittest.TestCase):
     def test_confidence_interval_with_sample_stats_normal(self):
         sample_proportion = 0.6
         sample_size = 31
@@ -108,6 +193,90 @@ class ProportionSamplingDistributionUnitTest(unittest.TestCase):
 
         sampling_distribution = ProportionSamplingDistribution(sample_distribution=SampleDistribution(sample,
                                                                                                       categorical_value="OK"))
+        self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.simulation)
+        print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
+              + ', standard_error = ' + str(sampling_distribution.standard_error) + ')')
+        print('confidence level for 95% confidence level: ' + str(sampling_distribution.confidence_interval(0.95)))
+
+
+class ProportionDiffSamplingDistributionUnitTest(unittest.TestCase):
+    def test_confidence_interval_with_sample_stats_normal(self):
+        grp1_sample_proportion = 0.6
+        grp1_sample_size = 31
+        grp2_sample_proportion = 0.51
+        grp2_sample_size = 32
+        sampling_distribution = ProportionDiffSamplingDistribution(grp1_sample_proportion=grp1_sample_proportion,
+                                                                   grp1_sample_size=grp1_sample_size,
+                                                                   grp2_sample_proportion=grp2_sample_proportion,
+                                                                   grp2_sample_size=grp2_sample_size)
+        self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.normal)
+        print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
+              + ', standard_error = ' + str(sampling_distribution.standard_error) + ')')
+        print('confidence level for 95% confidence level: ' + str(sampling_distribution.confidence_interval(0.95)))
+
+    def test_confidence_interval_with_sample_normal(self):
+        grp1_sample = Sample()
+        grp2_sample = Sample()
+
+        for i in range(100):
+            if random() <= 0.6:
+                grp1_sample.add_category("OK")
+            else:
+                grp1_sample.add_category("CANCEL")
+
+        for i in range(100):
+            if random() <= 0.61:
+                grp2_sample.add_category("OK")
+            else:
+                grp2_sample.add_category("CANCEL")
+
+        sampling_distribution = ProportionDiffSamplingDistribution(grp1_sample_distribution=SampleDistribution(
+            grp1_sample, categorical_value="OK"),
+            grp2_sample_distribution=SampleDistribution(
+                grp2_sample, categorical_value="OK"))
+        self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.normal)
+        print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
+              + ', standard_error = ' + str(sampling_distribution.standard_error) + ')')
+        print('confidence level for 95% confidence level: ' + str(sampling_distribution.confidence_interval(0.95)))
+
+    def test_confidence_interval_with_sample_stats_simulation(self):
+        grp1_sample_proportion = 0.6
+        grp1_sample_size = 10
+        grp2_sample_proportion = 0.61
+        grp2_sample_size = 9
+        sampling_distribution = ProportionDiffSamplingDistribution(grp1_sample_proportion=grp1_sample_proportion,
+                                                                   grp1_sample_size=grp1_sample_size,
+                                                                   grp2_sample_proportion=grp2_sample_proportion,
+                                                                   grp2_sample_size=grp2_sample_size
+                                                                   )
+        self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.simulation)
+        print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
+              + ', standard_error = ' + str(sampling_distribution.standard_error) + ')')
+        print('confidence level for 95% confidence level: ' + str(sampling_distribution.confidence_interval(0.95)))
+
+    def test_confidence_interval_with_sample_simulation(self):
+        grp1_sample = Sample()
+        grp2_sample = Sample()
+
+        for i in range(10):
+            if random() <= 0.6:
+                grp1_sample.add_category("OK")
+            else:
+                grp1_sample.add_category("CANCEL")
+
+        for i in range(9):
+            if random() <= 0.61:
+                grp2_sample.add_category("OK")
+            else:
+                grp2_sample.add_category("CANCEL")
+
+        sampling_distribution = ProportionDiffSamplingDistribution(
+            grp1_sample_distribution=SampleDistribution(grp1_sample,
+                                                        categorical_value="OK"),
+            grp2_sample_distribution=SampleDistribution(
+                grp2_sample,
+                categorical_value="OK")
+            )
         self.assertEqual(sampling_distribution.distribution_family, DistributionFamily.simulation)
         print('sampling distribution: (point_estimate = ' + str(sampling_distribution.point_estimate)
               + ', standard_error = ' + str(sampling_distribution.standard_error) + ')')
